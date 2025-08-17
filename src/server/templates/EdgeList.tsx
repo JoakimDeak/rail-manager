a0f0eb66d1e581f8a8a9b728b95966d4fc7e76b5
opera-joakimd <joakimd@opera.com>
2025-08-17 02:03:36 +0200

    todos

--- src/server/templates/EdgeList.tsx
+++ src/server/templates/EdgeList.tsx
@@ -7,6 +7,7 @@ export const EdgeList = ({ oobSwap }: { oobSwap?: string }) => {
     const [a, b] = network.nodes.filter((node) => edge.nodes.includes(node.id))
     return { ...edge, nodes: [a, b] }
   })
+  // TODO: Only do min h if theres at least one edge
   return (
     <ul
       class="no-scrollbar flex min-h-10 flex-col gap-2 overflow-y-scroll pt-px pr-0.5 pb-0.5"
--- src/server/templates/NodeList.tsx
+++ src/server/templates/NodeList.tsx
@@ -2,6 +2,7 @@ import { Network } from 'server/network'
 import { Node } from './Node'
 
 export const NodeList = ({ nodes }: { nodes: Network['nodes'] }) => {
+  // TODO: Only do min h if theres at least one node
   return (
     <ul class="no-scrollbar flex min-h-10 flex-col gap-2 overflow-y-scroll pt-px pr-0.5 pb-0.5">
       {nodes.map((node) => (
